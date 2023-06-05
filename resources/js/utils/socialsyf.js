fdc4fe8911fc2eccd29dd56193ff172503a65ee6
yfernandez05 <yhonatanfernandezcardenas@gmail.com>
2023-06-05 17:07:49 -0500

    update responsive show popupWhatsapp and the optimization animation shows messages

--- resources/js/utils/socialsyf.js
+++ resources/js/utils/socialsyf.js
@@ -1,8 +1,6 @@
-//acrive sound and notification
+//active sound and notification
 document.addEventListener('DOMContentLoaded', function() {
     var badge = document.querySelector('.badge-wtsp');
-    
-  
     setTimeout(function() {
         badge.classList.remove('hide');
         var notificationSound = document.getElementById('notificationSound');
@@ -11,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log('-');
         });
     }, 5000);
-
-  });
+});
   
-  //accion al dar clic al boton
-  document.querySelector('.btn-whatsapp').addEventListener('click', function(event) {
+//show animation tipyn and show info mensaje
+var setTimeoutExecuted = false;
+document.querySelector('.btn-whatsapp').addEventListener('click', function(event) {
     event.preventDefault();
     var popupContent = document.querySelector('.popup-content');
     popupContent.classList.toggle('hide');
-
+  
     var btnWhatsApp = this;
     var icon = btnWhatsApp.querySelector('i');
   
@@ -28,17 +26,30 @@ document.addEventListener('DOMContentLoaded', function() {
     icon.classList.toggle('fa-times');
     icon.classList.toggle('fab');
     icon.classList.toggle('fas');
+  
+    var messageElements = document.querySelectorAll('[class^="message-text"]');
 
-    setTimeout(function() {
-        var chatBackground = document.querySelector('.chat-background');
-        chatBackground.classList.add('hide');
+    if (!setTimeoutExecuted) {
+        setTimeoutExecuted = true;
 
-        var messageText = document.querySelector('.message-text');
-        messageText.classList.remove('hide');
-    }, 800);
-    setTimeout(function() {
-        var messageText = document.querySelector('.message-text-2');
-        messageText.classList.remove('hide');
-    }, 1500);
-      
+        setTimeout(async function() {
+            var chatBackground = document.querySelector('.chat-background');
+            chatBackground.classList.add('hide');
+  
+            //mostrar los mensajes sucesivamente con un retraso ajustable
+            async function showMessages() {
+                let delay = 0; // retaso primer mensaje
+        
+                for (let i = 0; i < messageElements.length; i++) {
+                    await new Promise(resolve => setTimeout(resolve, delay));
+                    messageElements[i].classList.remove('hide');
+                    delay = 900; // Ajusta el retraso desde el segundo mensajes
+                    //console.log('mensaje Nº ' + i);
+                }
+            }    
+            await showMessages();
+
+        }, 800);
+    }
+    
   });
--- resources/views/partials/utils/floatingsocialyf.blade.php
+++ resources/views/partials/utils/floatingsocialyf.blade.php
@@ -3,40 +3,39 @@
 </audio>
 <div class="float-socials">
     <!-- body chat -->
-    <div class="whatsapp-popup col-12 col-md-10 col-lg-11 col-xl-9">
+    <div class="whatsapp-popup">
         <div class="popup-content bg-white hide">
-            <div class="">
+            <div class="cont-header">
                 <div class="popup-header bg-primary px-4 py-3 d-flex align-items-center">
                     <div class="profile-image">
-                        <img src="{{asset('images/miningalati-default.png')}}" alt="" class="img-fluid">
+                        <img src="{{asset('images/favicon.png')}}" alt="" class="img-fluid">
                     </div>
                     <div class="profile-info text-white">
-                        <h5 class="m-0">Asistente Mining Alati</h5>
+                        <h5 class="m-0">Asistente Whatsapp</h5>
                         <h6 class="m-0 font-secondary">Escríbenos y te responderemos</h6>
                     </div>
                 </div>
-                <div class="popup-body p-3 pl-4 pt-2">
+                <div class="popup-body p-3 pl-4 pt-2 position-relative">
                     <div class="chat-background shadow-sm"><p class="mb-0 p-0" style="width: 350px;"></p>
                         <div class="typing-animation col-12"></div>
                     </div>
                     <div class="message-text hide whatsapp-message shadow-sm mb-2">
-                        <h5 class="mb-2"><small class="text-muted font-secondary">Asistente Mining Alati</small></h5>
+                        <h5 class="mb-2"><small class="text-muted font-secondary">Asistente Whatsapp</small></h5>
                         <p class="mb-1">
-                            Bienvenido a Mining Alati Cursos de Especialización Minera 👷
+                            Bienvenido a esta pagina de Cursos de Especialización 👷
                         </p>
                         <p class="mb-1">
                             ¿En qué podemos ayudarte? 👇
                         </p>
-
                     </div>
 
-                    <div class="message-text-2 hide whatsapp-message shadow-sm mb-1">
-                        <h5 class="mb-2"><small class="text-muted font-secondary">Asistente Mining Alati</small></h5>
+                    <div class="message-text hide whatsapp-message shadow-sm mb-2">
+                        <h5 class="mb-2"><small class="text-muted font-secondary">Asistente Whatsapp</small></h5>
                         <p class="mb-1">
                             ¿cómo puedo serparar cupon de atención? <a href="#" class="font-primary">AQUÍ</a>
                         </p>
                         <p class="mb-1">
-                            ¿Más información sobre las especializaciones mineras? <a href="#" class="font-primary">AQUÍ</a>
+                            ¿Más información sobre las especializaciones? <a href="#" class="font-primary">AQUÍ</a>
                         </p>
                         <p class="mb-1">
                             ¿Quieres contactarte con un asesor? <a href="#" class="font-primary">AQUÍ</a>
